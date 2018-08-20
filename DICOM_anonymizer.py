b63dc4ef960f0a5de9c25665192835f427d14fb7
aledelmo <delmonte.ale92@gmail.com>
2018-08-20 10:09:14 +0200

    Automatically distinguish dcm files and keep diffusion information.

--- DICOM_anonymizer.py
+++ DICOM_anonymizer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import argparse
 import os.path
 import sys
-
+import random
+import string
 import pydicom
+from builtins import range
 
 try:
     from os import scandir, walk
@@ -12,97 +15,64 @@ except ImportError:
     from scandir import scandir, walk
 try:
     from joblib import Parallel, delayed, cpu_count
-    from psutil import virtual_memory
 
-    parallel = True
+    par = True
 except:
-    parallel = False
+    par = False
 
 __author__ = 'Alessandro Delmonte'
 __email__ = 'delmonte.ale92@gmail.com'
 
 
 def main():
-    dir_path, q, e = setup()
+    dir_path, q = setup()
 
     if q:
         sys.stdout = open(os.devnull, 'w')
         sys.stderr = open(os.devnull, 'w')
 
-    for root, _, files in walk(dir_path):
-        if not e:
-            if 'DICOM' in root:
-                if files:
-                    if parallel:
-                        num_cores = cpu_count()
-                        Parallel(n_jobs=num_cores)(delayed(anonymize_par)(root, img) for img in files)
-                    else:
-                        anonymize(root, files)
-        else:
-            if parallel:
-                num_cores = cpu_count()
-                Parallel(n_jobs=num_cores)(delayed(anonymize_par)(root, img) for img in files if img.endswith('.dcm'))
-            else:
-                anonymize(root, files, e)
+    if par:
+        print('Parallel computing enabled')
+        with Parallel(n_jobs=cpu_count(), backend='threading') as parallel:
+            parallel(delayed(anonymize)(root, files) for root, _, files in walk(dir_path))
+    else:
+        print('Parallel computing not available (install joblib)')
+        for root, _, files in walk(dir_path):
+            anonymize(root, files)
 
 
-def anonymize_par(directory, img):
-    try:
-        ds = pydicom.dcmread(os.path.join(directory, img))
-        ds.remove_private_tags()
-        ds.walk(type1_callback)
-        ds.walk(type2_callback)
-        ds.walk(type3_callback)
+def anonymize(root, files):
+    files = [os.path.join(root, file) for file in files]
+    random_string = ''.join(random.choice(string.ascii_uppercase) for _ in range(8))
+    for file in files:
+        try:
+            ds = pydicom.dcmread(file)
 
-        type4_tags = ['OtherPatientIDsSequence']
-        for tag in type4_tags:
-            if tag in ds:
-                delattr(ds, tag)
+            ds.walk(del_callback)
 
-        os.remove(os.path.join(directory, img))
-        ds.save_as(os.path.join(directory, img))
-    except:
-        print 'Non DICOM file found in {}'.format(os.path.normpath(directory))
+            ds.data_element('PatientName').value = random_string
+            ds.data_element('PatientID').value = "(??)"
+            ds.data_element('InstitutionName').value = "(??)"
+            ds.data_element('SeriesDescription').value = "(??)"
+            ds.data_element('ProtocolName').value = "(??)"
 
+            for tag in ['PatientWeight', 'AdditionalPatientHistory']:
+                if tag in ds:
+                    delattr(ds, tag)
 
-def anonymize(directory, files, e=False):
-    for img in files:
-        if e and img.endswith('.dcm'):
-            try:
-                ds = pydicom.dcmread(os.path.join(directory, img))
-                ds.remove_private_tags()
-                ds.walk(type1_callback)
-                ds.walk(type2_callback)
-                ds.walk(type3_callback)
-
-                type4_tags = ['OtherPatientIDsSequence']
-                for tag in type4_tags:
-                    if tag in ds:
-                        delattr(ds, tag)
-
-                os.remove(os.path.join(directory, img))
-                ds.save_as(os.path.join(directory, img))
-            except:
-                print 'Non DICOM file found in {}'.format(os.path.normpath(directory))
+            os.remove(file)
+            ds.save_as(file)
+        except:
+            pass
 
 
-def type1_callback(ds, data_element):
-    type1_tags = ['PN']
-    for tag in type1_tags:
-        if data_element.VR == tag:
-            data_element.value = "Anonymous"
-
-
-def type2_callback(ds, data_element):
+def del_callback(ds, data_element):
+    if data_element.VR == 'PN':
+        data_element.value = 'ANONYMOUS'
     if data_element.VR == 'DA':
         data_element.value = "20000101"
     if data_element.VR == 'TM':
         data_element.value = "000000"
-
-
-def type3_callback(ds, data_element):
-    if data_element.VR == 'LO':
-        data_element.value = "(??)"
     if data_element.VR == 'SH':
         data_element.value = "(??)"
 
@@ -110,13 +80,11 @@ def type3_callback(ds, data_element):
 def setup():
     parser = argparse.ArgumentParser()
     parser.add_argument('DICOM_Folder', help='Database to anonimyze', type=check_folder)
-    parser.add_argument('-e', '--ext', help='Anonymize by file extension (.dcm, default: by folder name)',
-                        action='store_true')
     parser.add_argument('-q', '--quiet', help='Suppress output', action='store_true')
 
     args = parser.parse_args()
 
-    return args.DICOM_Folder, args.quiet, args.ext
+    return args.DICOM_Folder, args.quiet
 
 
 def check_folder(value):
